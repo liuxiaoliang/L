fb8d1b7c32fd9a31e332f796d24734e2c5166b4f
liuxiaoliang <leon.max.liew@gmail.com>
2014-07-10 15:26:20 +0800

    rntn code

--- mllib/classify/rntn4vec.py
+++ mllib/classify/rntn4vec.py
@@ -16,6 +16,7 @@ sys.path.append("../../common")
 import math
 import random
 import numpy as np
+import data_prepare as dpp
 
 class RNTN(object):
     """rnn model
@@ -23,53 +24,83 @@ class RNTN(object):
     """
     
     def __init__(self, op=None, model=None, 
-                 feature_file=None, dev_file=None, 
+                 feature_file=None, dev_file=None,
                  train_file=None, model_file=None):
         self.op = op # training options
         self.model = model # model parameters
         self.feature_file = feature_file
         self.dev_file = dev_file
         self.train_file = train_file
-        self.model_file = model_file
-        
-        self.t = Timing()
-        
-    def develop(self):
-        """adjust parameters
+        self.model_file = model_file        
+        self.t = Timing() # Timing class
+        self.trd = None # train or test dpp handle
+        self.drd = None # dev dpp handle
+
+    def load_data(self):
+        """Load data
 
         """
-        pass
-    
+        # read trees
+        if self.train_file:
+            self.trd = dpp.RNNDPP(self.feature_file, self.train_file)
+            self.trd.load_feature()
+            self.trd.load_sample()
+        if self.dev_file:
+            self.drd = dpp.RNNDPP(self.feature_file, self.dev_file)
+            self.drd.load_feature()
+            self.drd.load_sample()
+        
     def train(self):
         """train wordvec and classified model
 
         """
-        training_trees = 
-        sum_grad_square = [] # init sum_grad_square
-        batches_num = training_trees.size() / float(self.batch_size) + 1;
-        
+        self.t.start()
+        sum_grad_square = [0.0] * self.model.total_param_size() # init sum_grad_square
+        if self.op.batch_size == 0:
+            self.op.batch_size = 1
+        batches_num = int(len(self.trd.slist) / float(self.op.batch_size));
         # adagrad
-        for epoch in range(self.epochs):
+        for epoch in range(self.op.epochs):
             print "starting epoch %d" % epoch
-            if (epoch > 0 and self.adagrad_reset_frequency > 0 and 
-                (epoch % self.adagrad_reset_frequency) == 0):
-                sum_grad_square = [] # reset sum_grad_square
-            shuffle(training_trees)
+            if (epoch > 0 and self.op.adagrad_reset_frequency > 0 and 
+                (epoch % self.op.adagrad_reset_frequency) == 0):
+                sum_grad_square = [0.0] * self.model.total_param_size() # reset sum_grad_square
+            shuffle_tree_list = self.trd.slist
+            random.shuffle(shuffle_tree_list)
             for batch in range(batches_num):
                 print "Epoch %d, batch %d" % (epoch, batch)
-                tree_start = self.batch_size * batch
-                tree_end = self.batch_size * (batch + 1)
-                if tree_end + self.batch_size > train_trees.size():
-                    tree_end = train_trees.size()
-                self.one_batch_traning(self.rnnloss, training_trees[tree_start, tree_end], sum_grad_square)
-                
-                    
-    
-    def one_batch_training(self, rnnloss, rnntree, sum_grad_square):
-        # adagrad
-        for t in rnntree:
+                tree_start = self.op.batch_size * batch
+                tree_end = self.op.batch_size * (batch + 1)
+                if tree_end + self.op.batch_size > len(shuffle_tree_list):
+                    tree_end = len(shuffle_tree_list)
+                self.one_batch_traning(self.model, shuffle_tree_list[tree_start, tree_end], sum_grad_square)
+                # computer timing
+                elapsed_time = self.t.report()
+                print "epoch is %d, batch is %d, elapsed time %d" % (epoch, batch, elapsed_time)
+                if(self.op.max_train_time_seconds > 0 and elapsed_time > self.op.max_train_time_seconds*1000):
+                    break
+                if (batch==0 and epoch > 0 and epoch % self.op.debug_output_epochs == 0):
+                    score = 0.0
+                    pass
+            elapsed_time = self.t.report()
+            if(self.op.max_train_time_seconds > 0 and elapsed_time > self.op.max_train_time_seconds*1000):
+                print "Max training time exceeded, exiting"
+                break
             
-
+    def one_batch_training(self, t, sum_grad_square):
+        # adagrad
+        rlg = RntnLossAndGradient(self.model,t)
+        theta = self.model.param2vector()
+        eps = 1e-3
+        cur_loss = 0
+        gradf = rlg.derivative(theta)
+        cur_loss = rlg.value(theta)
+        print "cur batch loss %f" % cur_loss
+        for f in range(len(gradf)):
+            sum_grad_square[f] = sum_grad_square[f] + gradf[f]*gradf[f]
+            theta[f] = theta[f] - (self.op.learning_rate * gradf[f]/(math.sqrt(sum_grad_square[f])+eps))
+        self.model.vector2param(theta)    
+        
     def wordvec(self):
         """get distributed represent for sentence
 
@@ -124,7 +155,7 @@ class RntnOptions(object):
     def __init__(self, batch_size=27, epochs=400, debug_output_epochs=8, 
                  max_train_time_seconds=60*60*24, learning_rate=0.1, 
                  adagrad_reset_frequency=1, regW=0.001, regV=0.001, 
-                 regWs=0.0001, regL=0.0001):
+                 regWs=0.0001, regL=0.0001, isEval=True):
         self.batch_size = batch_size # batch size in each training
         self.epochs = epochs # training epochs
         self.debug_output_epochs = debug_output_epochs
@@ -136,19 +167,24 @@ class RntnOptions(object):
         self.regWs = regWs # Regularization cost for the classification matrices
         self.regL = regL # Regularization cost for the word vectors
 
-        
+
 class RntnModel(object):
     """model parameters
 
     """
-    def __init__(self, num_cate=5, num_hid=25):
+    def __init__(self, num_cate=5, num_hid=25, feature_file=None):
         self.num_cate = num_cate # number of calsses
         self.num_hid = num_hid # Dimension of hidden layers, size of word vectors
         self.W = np.zeros(shape=(num_hid, 2*num_hid + 1), dtype=float) # transform
         self.V = np.zeros(shape=(2*num_hid, 2*num_hid, num_hid), dtype=float) # tensor
         self.Ws = np.zeros(shape=(num_cate, num_hid + 1), dtype=float) # cate weight
         self.L = {} # num_word * num_hid
-    
+        rd = dpp.RNNDPP(feature_file, '')
+        rd.load_feature()
+        # init L in model
+        for fid in rd.id2feature:
+            self.model.L[fid] = np.zeros(shape=(num_hid, 1), dtype=float)
+            
     def randomW(self):
         # bias column values are initialized zero
         r = 1.0 / (math.sqrt(self.num_hid)*2.0)
@@ -177,6 +213,9 @@ class RntnModel(object):
         for w in self.L:
             for i in range(self.num_hid):
                 self.L[w][i,0] = random.gauss(0,1)
+    
+    def total_param_size(self):
+        return self.W.size + self.V.size + self.Ws.size + len(self.L) * self.num_hid
 
 
 class RntnLossAndGradient(object):
@@ -214,7 +253,7 @@ class SomeFunc(object):
         return out
 
 
-def rntn_train(dev_file, train_file, model_file):
+def rntn_train(feature_file, dev_file, train_file, model_file):
     pass
 
 
