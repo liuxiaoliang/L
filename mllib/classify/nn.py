fed7a1eef5114e5fa16490037abd7a26071e8bd1
liuxiaoliang <leon.max.liew@gmail.com>
2014-08-28 17:17:48 +0800

    add nn

---
+++ mllib/classify/nn.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright (c) <2014> <leon.max.liew@gmail.com>
+# 
+
+"""neural network
+
+Because nn is a online algorithm, it's training procedure is going in
+some specific application unlike bayes and so on.
+
+Algo:
+1) Averaged Perceptron
+
+"""
+
+class Perceptron(object):
+    
