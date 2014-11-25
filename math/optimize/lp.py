faca3aec5a985702db58751da7bc3d99f63899cc
liuxiaoliang <leon.max.liew@gmail.com>
2014-11-25 17:07:23 +0800

    test

---
+++ math/optimize/lp.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright (c) <2014> <leon.max.liew@gmail.com> 
+#
+
+"""linear programming
+
+simple method
+"""
+
+__author__ = 'xiaoliang liu'
+
+import os
+import sys
+import numpy as np
+
+class SimplexMethodBase(object):
+    """simplex method
+
+    """
+    def __init__(self, ):
+        pass
+
+    def solve(self):
+        pass
+
+class SimplexMethodByGauss(SimplexMethodBase):
+    """gauss elimination
+    
+    """
+    def __init__(self):
+        SimplexMethodBase.__init__(self,)
+
+    
+
+if __name__ == '__main__':
+    print 'test'
